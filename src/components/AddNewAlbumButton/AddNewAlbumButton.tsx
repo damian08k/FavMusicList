68bdad656e8e8c4aeb8230c572e1759da52a00d9
damian08k <damiankierzkowski08@gmail.com>
2023-05-04 13:45:42 +0200

    feat: Add view changing buttons

--- src/components/AddNewAlbumButton/AddNewAlbumButton.tsx
+++ src/components/AddNewAlbumButton/AddNewAlbumButton.tsx
@@ -3,7 +3,7 @@ import { PlusIcon } from "./components/PlusIcon/PlusIcon";
 
 export const AddNewAlbumButton = () => {
   return (
-    <button className={classes.root}>
+    <button className={classes.root} aria-label="Add new album">
       <PlusIcon />
       <span className={classes.buttonText}>Add new album</span>
     </button>
--- src/components/AlbumsList/AlbumsList.tsx
+++ src/components/AlbumsList/AlbumsList.tsx
@@ -1,10 +1,14 @@
 import classes from "./AlbumsList.module.css";
+import { ListView } from "./components/ListView/ListView";
 import { SortedBy } from "./components/SortedBy/SortedBy";
 
 export const AlbumsList = () => {
   return (
     <div className={classes.root}>
-      <SortedBy />
+      <section className={classes.topListSection}>
+        <SortedBy />
+        <ListView />
+      </section>
     </div>
   );
 };
